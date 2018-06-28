1f036ee27d9ae786124c5d48a47b4b7df3d61630
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-06-28 12:41:24 +1000

    The very basics addded

---
+++ visual.js
@@ -0,0 +1,76 @@
+function Canvas(id) {
+    this.cnv = document.querySelector('canvas#' + id);
+
+    this.ctx = this.cnv.getContext('2d');
+    this.width = this.cnv.width;
+    this.height = this.cnv.height;
+
+    this.resize = function(w, h) {
+        this.width = w;
+        this.height = h;
+
+        this.cnv.setAttribute('width', w);
+        this.cnv.setAttribute('height', h);
+    }
+
+    this.fillRect = function(x, y, w, h) {
+        this.ctx.fillRect(x, y, w, h);
+    }
+
+    this.strokeRect = function(x, y, w, h) {
+        this.ctx.strokeRect(x, y, w, h);
+    }
+
+    this.background = function() {
+        this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
+    this.color = function(color) {
+        this.ctx.fillStyle = color;
+        this.ctx.strokeStyle = color;
+    }
+
+    this.lineWidth = function(width) {
+        this.ctx.lineWidth = width;
+    }
+}
+
+var SETUPS = [];
+var UPDATES = [];
+
+function beginLoop() {
+    var frameId = 0;
+    var lastFrame = Date.now();
+
+    function loop() {
+        var thisFrame = Date.now();
+        var elapsed = thisFrame - lastFrame;
+
+        frameId = window.requestAnimationFrame(loop);
+
+        for (var u of UPDATES) {
+            u(elapsed);
+        }
+
+        lastFrame = thisFrame;
+    }
+
+    loop();
+}
+
+function addUpdate(f) {
+    UPDATES.push(f);
+}
+
+function addSetup(f) {
+    SETUPS.push(f);
+}
+
+window.onload = function() {
+
+    for (var s of SETUPS) {
+        s();
+    }
+
+    beginLoop();
+};
