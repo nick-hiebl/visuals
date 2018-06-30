c6677e3a14235e1f851e147875f0ca9ebf0f92db
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-01 01:00:06 +1000

    Correctly implemented Prim's algorithm

--- mst/prim.js
+++ mst/prim.js
@@ -1,6 +1,8 @@
 var connected = [];
+var cheapest = [];
+var from = [];
 var pEdges;
-var bestEdge = undefined;
+var nextBest;
 var considering;
 var pCanvas;
 
@@ -8,10 +10,12 @@ function setupPrim() {
     pCanvas = new Canvas('prim');
     for (var i = 0; i < num; i++) {
         connected.push(false);
+        cheapest.push(dist(points[0], points[i]));
+        from.push(0);
     }
     connected[0] = true;
-    considering = {node: 0, edge: 1};
-    bestEdge = {node: 0, edge: 1, dist: 10000};
+    considering = 0;
+    nextBest = 0;
     pEdges = [];
 }
 
@@ -19,37 +23,52 @@ function updatePrim() {
     pCanvas.color('black');
     pCanvas.background();
 
-    pCanvas.lineWidth(2);
+    if (pEdges.length < (num - 1)) {
+        pCanvas.lineWidth(1);
+        pCanvas.color('green');
+        for (var n = 0; n < num; n++) {
+            if (!connected[n]) {
+                pCanvas.line(points[n].x, points[n].y,
+                    points[from[n]].x, points[from[n]].y);
+            }
+        }
 
-    if (considering.edge >= num) {
-        considering.edge = 0;
-        considering.node++;
-        while (considering.node < num && !connected[considering.node]) {
-            considering.node++;
+        while (considering < num && connected[considering]) {
+            considering++;
+        }
+
+        if (considering >= num) {
+            connected[nextBest] = true;
+            pEdges.push({a: from[nextBest], b: nextBest});
+            considering = 0;
+            for (var n = 0; n < num; n++) {
+                let d = dist(points[nextBest], points[n]);
+                if (d < cheapest[n]) {
+                    cheapest[n] = d;
+                    from[n] = nextBest;
+                }
+            }
+        } else {
+            if (!connected[considering]) {
+                let d = dist(points[considering],
+                    points[from[considering]]);
+                if (connected[nextBest] || d < cheapest[nextBest]) {
+                    nextBest = considering;
+                }
+            }
+            pCanvas.color('red');
+            pCanvas.lineWidth(2);
+            pCanvas.line(points[considering].x,
+                points[considering].y, points[from[considering]].x,
+                points[from[considering]].y);
+            pCanvas.color('green');
+            pCanvas.fillArc(points[nextBest].x,
+                points[nextBest].y, 9);
+            considering++;
         }
     }
-    if (considering.node >= num) {
-        pEdges.push({a: bestEdge.node, b: bestEdge.edge});
-        connected[bestEdge.b] = true;
-        bestEdge = {node: 0, edge: 1, dist: 10000};
-        considering.edge = 0;
-        considering.node = 1;
-    }
 
-    d = dist(points[considering.edge], points[considering.node]);
-    if (!connected[considering.other] && d < bestEdge.dist) {
-        bestEdge = {node: considering.node, edge: considering.edge,
-            dist: d};
-    }
-
-    if (bestEdge != undefined) {
-        pCanvas.color('red');
-        pCanvas.line(points[bestEdge.node].x,
-            points[bestEdge.node].y,
-            points[bestEdge.edge].x,
-            points[bestEdge.edge].y);
-    }
-
+    pCanvas.lineWidth(2);
     pCanvas.color('white');
 
     for (var e of pEdges) {
