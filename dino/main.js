5efec414d57dd4663e374a46d32019adbb42684b
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-05 11:14:45 +1000

    Implemented automatic jumping

--- dino/main.js
+++ dino/main.js
@@ -7,6 +7,9 @@ var offset;
 var last;
 
 var speed = 6;
+var jumpDistance = 50;
+var jumpVelocity = 12;
+var gravity = 0.625;
 
 function setup() {
     initGame();
@@ -17,12 +20,14 @@ function setup() {
 }
 
 function addObstacle() {
-    var x = last + 200 + Math.floor(Math.random() * 400);
+    var x = last + 300 + Math.floor(Math.random() * 300);
 
     last = x;
     obstacles.push({
         x: x,
-        y: 0
+        y: 0,
+        w: Math.floor(20 + Math.random() * 30),
+        h: Math.floor(40 + Math.random() * 30)
     });
 }
 
@@ -35,7 +40,9 @@ function initGame() {
 
     dino = {
         x: 0,
-        y: 0
+        y: 0,
+        vx: speed,
+        vy: 0
     };
     offset = {
         x: 100,
@@ -45,7 +52,24 @@ function initGame() {
 
 function update() {
     offset.x -= speed;
-    dino.x += speed;
+    dino.x += dino.vx;
+
+    dino.y += dino.vy;
+    if (dino.y < 0) {
+        dino.vy += gravity;
+    }
+    if (dino.y > 0) {
+        dino.y = 0;
+        dino.vy = 0;
+    }
+    if (dino.y == 0) {
+        for (var o of obstacles) {
+            if (dino.x < o.x && dino.x + jumpDistance >= o.x) {
+                dino.vy -= jumpVelocity;
+                break;
+            }
+        }
+    }
 
     draw();
 }
@@ -58,8 +82,15 @@ function draw() {
     canvas.color('green');
     canvas.fillRect(dino.x + offset.x, dino.y + offset.y, -50, -80);
     canvas.color('red');
-    for (var o of obstacles) {
-        canvas.fillRect(o.x + offset.x, o.y + offset.y, 50, -80);
+    for (var i = 0; i < obstacles.length; i++) {
+        var o = obstacles[i];
+        if (o.x + o.w + offset.x < 0) {
+            obstacles.splice(i, 1);
+            i--;
+            addObstacle();
+        } else {
+            canvas.fillRect(o.x + offset.x, o.y + offset.y, o.w, -o.h);
+        }
     }
 }
 
