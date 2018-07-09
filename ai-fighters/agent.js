1bbfb9f4074281604b24e94ea1c77c31d2d4d3e1
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-09 19:14:17 +1000

    Added 'ranges' for detection which are now drawn

--- ai-fighters/agent.js
+++ ai-fighters/agent.js
@@ -1,8 +1,39 @@
 var Agent = function(x, y) {
     this.position = new Vector(x, y);
     this.heading = 0;
+    this.zones = [{
+        a: 0,
+        r: 1,
+        l: Math.random() * 50 + 100
+    }, {
+        a: Math.random() * Math.PI * 2,
+        r: Math.random(),
+        l: Math.random() * 50 + 100
+    }, {
+        a: Math.random() * Math.PI * 2,
+        r: Math.random(),
+        l: Math.random() * 50 + 100
+    }];
 
     this.draw = function(canvas) {
+        canvas.lineWidth(1);
+        canvas.color('green');
+        for (var zone of this.zones) {
+            var zone_r = zone.l;
+            v = new Vector(zone_r, 0);
+            v.heading = this.heading + zone.a - zone.r/2;
+            v.add(this.position);
+            canvas.line(this.position.x, this.position.y, v.x, v.y);
+
+            v = new Vector(zone_r, 0);
+            v.heading = this.heading + zone.a + zone.r/2;
+            v.add(this.position);
+            canvas.line(this.position.x, this.position.y, v.x, v.y);
+
+            canvas.strokeArc(this.position.x, this.position.y,
+                zone_r, zone_r, 0, this.heading + zone.a - zone.r/2, this.heading + zone.a + zone.r/2);
+        }
+
         canvas.color('red');
         canvas.fillArc(this.position.x, this.position.y, 10);
         var v = new Vector(20, 0);
