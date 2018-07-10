483683659d425776b77eb70119df9f7e506febb5
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-10 11:54:28 +1000

    Changed to testing for zones

--- ai-fighters/main.js
+++ ai-fighters/main.js
@@ -1,19 +1,32 @@
 var canvas;
 var agent;
+var target;
+
+var t;
 
 function setup() {
     canvas = new Canvas("canvas");
     agent = new Agent(canvas.width/2, canvas.height/2);
+    t = -1;
+    target = {
+        position: new Vector(300, 300)
+    };
 
     addUpdate(update);
 }
 
 function update() {
-    agent.turn(-0.01);
-    // agent.forward(3);
-    agent.strafe(3);
-    agent.boundary(canvas.width, canvas.height, 2);
-    agent.update();
+    t += 0.003;
+    // agent.turn(Math.sin(0.3282 * t) / 120 + Math.sin(t) / 100 + Math.sin(2.8834 * t) / 200);
+    // agent.forward(0.5 + Math.cos(t));
+    // agent.strafe(0.5 - Math.cos(t));
+    // agent.boundary(canvas.width, canvas.height, 2);
+    target.position.heading = t;
+    // console.log(target.position.heading);
+    target.position.magnitude = 100 + (Math.sin(t * 0.4) * 0.4 + Math.sin(t) * 0.6) * 80;
+    target.position.x += 400;
+    target.position.y += 400;
+    agent.update(target);
 
     draw();
 }
@@ -26,6 +39,9 @@ function draw() {
     canvas.fillRect(1, 1, canvas.width-2, canvas.height-2);
 
     agent.draw(canvas);
+
+    canvas.color('black');
+    canvas.fillArc(target.position.x, target.position.y, 3);
 }
 
 addSetup(setup);
