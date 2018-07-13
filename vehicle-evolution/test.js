2dd43138b7317a0d8ae2edeeed003ce8e14257ea
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-13 23:27:42 +1000

    Added basic food seeking

--- vehicle-evolution/test.js
+++ vehicle-evolution/test.js
@@ -1,23 +1,27 @@
 var canvas;
 var vehicle;
 
-var target;
+var food;
+
 var t;
 
 function setup() {
     canvas = new Canvas('canvas');
     vehicle = new Vehicle(canvas.width/2, canvas.height/2);
-    target = new Vector(300, 300);
+
+    food = [];
+    for (var i = 0; i < 20; i++) {
+        food.push(new Vector(Math.random() * canvas.width,
+                             Math.random() * canvas.height));
+    }
+
     t = 0;
     addUpdate(update);
 }
 
 function update() {
-    t += 0.01;
-    target.heading = Math.PI/4 + Math.sin(t) / 2;
-    // vehicle.seek(target);
     vehicle.boundary(canvas.width, canvas.height);
-    vehicle.flee(target, 100/target.dist(vehicle.position));
+    vehicle.seekNearest(food, 0.2);
     vehicle.roam();
     vehicle.update();
     draw();
@@ -26,8 +30,10 @@ function update() {
 function draw() {
     canvas.color('black');
     canvas.background();
-    canvas.color('white');
-    canvas.fillArc(target.x, target.y, 10);
+    canvas.color('green');
+    for (var item of food) {
+        canvas.fillArc(item.x, item.y, 3);
+    }
     vehicle.draw(canvas);
 }
 
