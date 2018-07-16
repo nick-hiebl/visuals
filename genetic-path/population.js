5e719020ec9ee1d4ff69449cac13b1c48a7e9f29
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-16 23:36:51 +1000

    Added generational development

--- genetic-path/population.js
+++ genetic-path/population.js
@@ -10,7 +10,27 @@ var Population = function(size, len) {
 
     this.update = function(states, n) {
         for (var i = 0; i < this.size; i++) {
-            states[i].pos.add(this.pop[i].getStep(n));
+            if (states[i].alive)
+                states[i].pos.add(this.pop[i].getStep(n));
         }
+    }
+
+    this.rate = function(i, v) {
+        this.pop[i].rating = v;
+    }
+
+    this.nextGeneration = function() {
+        this.generations++;
+        this.pop.sort(function(a, b) {
+            return -(a.rating - b.rating);
+        });
+        var newPop = [];
+        newPop.push(this.pop[0]);
+        while (newPop.length < this.size) {
+            var r = Math.random() * Math.random();
+            var m = Math.floor(r * this.size);
+            newPop.push(this.pop[m].child());
+        }
+        this.pop = newPop;
     }
 }
